6a185de390df0e134c4c7cd78f0b90033650e85f
Rafli Gobel <rafligobel02@gmail.com>
2025-11-06 01:36:54 +0800

    hapus accessor 'totalTunjangan' dan 'gajiBersih' untuk meningkatkan performa model

--- app/Models/Gaji.php
+++ app/Models/Gaji.php
@@ -56,51 +56,8 @@ class Gaji extends Model
     {
         return $this->belongsTo(\App\Models\TunjanganKehadiran::class, 'tunjangan_kehadiran_id');
     }
-    
+
     public function penilaianKinerjas()
     {
         return $this->hasMany(PenilaianKinerja::class);
     }
-    /*
-    |--------------------------------------------------------------------------
-    | [PERBAIKAN 2: HAPUS ACCESSOR]
-    |--------------------------------------------------------------------------
-    |
-    | Accessor 'totalTunjangan' dan 'gajiBersih' dihapus dari model ini.
-    | Alasan: Memanggil service dari dalam model accessor menyebabkan masalah
-    | performa serius (N+1 Problem) dan membuat model menjadi lambat.
-    |
-    | Kalkulasi total ini sudah ditangani dengan benar di dalam controller
-    | (contoh: TenagaKerjaController dan LaporanController) sebelum data
-    | dikirim ke view. Itu adalah pendekatan yang jauh lebih efisien.
-    |
-    */
-}
-
-//     protected function totalTunjangan(): Attribute
-//     {
-//         // Mengambil tunjangan jabatan langsung dari relasi karyawan
-//         $tunjanganJabatan = $this->karyawan->jabatan->tunj_jabatan ?? 0;
-
-//         // Mengambil tunjangan kehadiran dari service (lebih akurat)
-//         $salaryService = app(\App\Services\SalaryService::class);
-//         $detailGaji = $salaryService->calculateDetailsForForm($this->karyawan, $this->bulan);
-//         $tunjanganKehadiran = $detailGaji['tunj_kehadiran'];
-
-//         return Attribute::make(
-//             get: fn() => $tunjanganKehadiran +
-//                 $tunjanganJabatan +
-//                 $this->tunj_anak +
-//                 $this->tunj_komunikasi +
-//                 $this->tunj_pengabdian +
-//                 $this->tunj_kinerja +
-//                 $this->lembur
-//         );
-//     }
-//     protected function gajiBersih(): Attribute
-//     {
-//         return Attribute::make(
-//             get: fn() => $this->gaji_pokok + $this->total_tunjangan - $this->potongan,
-//         );
-//     }
-// }
